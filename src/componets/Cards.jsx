0091970f4029d54a18db3c447e2b3b1339626142
Ben-Zard <zardusb@mail.gvsu.edu>
2022-09-06 23:50:09 -0400

    creating layout start

---
+++ src/componets/Cards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cards = () => {
+  return (
+    <div>
+      <img alt="robo" src="https://robohash.org/23.238.193.4.png" />
+      <h1>This is the start</h1>
+    </div>
+  );
+};
+
+export default Cards;
